6759ba7e326cd01e38be5ae2b0b761862b4d20eb
takeshisakuma <saude0518@gmail.com>
2019-12-31 16:43:19 +0900

    weather icon change svg

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ else {
 
 const myAppID = "156cfe5a2b6fbbd3c831f6386d4282aa";
 const myUnits = "metric";
+const myLang = "ja";
 let currentLat;
 let currentLon;
 
@@ -31,95 +32,138 @@ const getPosition = () => {
             currentLat = position.coords.latitude;
             currentLon = position.coords.longitude;
 
-            const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLat}&lon=${currentLon}&appid=${myAppID}&units=${myUnits}`;
+
+            const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${currentLat}&lon=${currentLon}&appid=${myAppID}&units=${myUnits}&lang=${myLang}`;
 
             //getWeather作成
             const getWeather = async () => {
-                const res = await fetch(URL);
+
+                const res = await fetch(forecastURL);
                 const data = await res.json();
                 return data;
+
             }
 
             //getWeather実行
             getWeather()
                 .then(data => {
 
-                    //天気id
-                    console.log(JSON.stringify(data.weather[0].id));
-
-                    //天気メイン
-                    console.log(JSON.stringify(data.weather[0].main));
-
-                    //天気解説
-                    console.log(JSON.stringify(data.weather[0].description));
-
-                    //天気アイコン
-                    console.log(JSON.stringify(data.weather[0].icon));
-
-                    //ベース
-                    console.log(JSON.stringify(data.base));
-
-                    //気温
-                    console.log(JSON.stringify(data.main.temp));
 
 
-                    //体感気温
-                    console.log(JSON.stringify(data.main.feels_like));
-
-                    //最低気温
-                    console.log(JSON.stringify(data.main.temp_min));
-
-                    //最高気温
-                    console.log(JSON.stringify(data.main.temp_max));
-
-                    //気圧(hPa)
-                    console.log(JSON.stringify(data.main.pressure));
-
-                    //湿度(%)
-                    console.log(JSON.stringify(data.main.humidity));
-
-                    //可視性
-                    console.log(JSON.stringify(data.visibility));
-
-                    //風速(meter/sec)
-                    console.log(JSON.stringify(data.wind.speed));
-
-                    //風向き
-                    console.log(JSON.stringify(data.wind.deg));
-
-                    //雲の割合？（Cloudiness in %）
-                    console.log(JSON.stringify(data.clouds.all));
-
-                    //データの時刻 GMTのunixtime形式(いわゆるEPOCH)
-                    console.log(JSON.stringify(data.dt));
 
 
-                    console.log(JSON.stringify(data.sys.type));
-                    console.log(JSON.stringify(data.sys.id));
-
-                    //国
-                    console.log(JSON.stringify(data.sys.country));
-
-                    //日の出時刻　unixtime
-                    console.log(JSON.stringify(data.sys.sunrise));
-
-                    //日の出時刻　日本
 
 
-                    //日の入り時刻 unixtime
-                    console.log(JSON.stringify(data.sys.sunset));
-
-                    //タイムゾーン　時差(秒)32400
-                    console.log(JSON.stringify(data.timezone));
-
-                    //地名のid？
-                    console.log(JSON.stringify(data.id));
-
-                    //地名
-                    console.log(JSON.stringify(data.name));
 
                     //ステータス 200以外はエラー？
-                    console.log(JSON.stringify(data.cod));
+                    console.log(data.cod);
+
+                    //メッセージ
+                    console.log(data.message);
+
+                    //取得する数
+                    console.log(data.cnt);
+
+                    //データ受信時刻
+                    console.log(data.list[0].dt);
+
+                    //気温
+                    console.log(data.list[0].main.temp);
+
+                    //体感気温
+                    console.log(data.list[0].main.feels_like);
+
+                    //最低気温
+                    console.log(data.list[0].main.temp_min);
+
+                    //最高気温
+                    console.log(data.list[0].main.temp_max);
+
+                    //気圧(hPa)
+                    console.log(data.list[0].main.pressure);
+
+                    //海面の気圧(hPa)
+                    console.log(data.list[0].main.sea_level);
+
+                    //地面の気圧(hPa)
+                    console.log(data.list[0].main.grnd_level);
+
+                    //湿度(%)
+                    console.log(data.list[0].main.humidity);
+                    console.log(data.list[0].main.temp_kf);
+
+                    //天気id
+                    console.log(data.list[0].weather[0].id);
+
+                    //天気main
+                    console.log(data.list[0].weather[0].main);
+
+                    //天気詳細
+                    console.log(data.list[0].weather[0].description);
+
+                    //天気アイコン
+                    console.log(data.list[0].weather[0].icon);
+
+                    //雲の割合？（Cloudiness in %）
+                    console.log(data.list[0].clouds.all);
+
+                    //風速(meter/sec)
+                    console.log(data.list[0].wind.speed);
+
+                    //風向き
+                    console.log(data.list[0].wind.deg);
+
+
+                    console.log(data.list[0].sys.pod);
+
+                    //日付と時刻
+                    console.log(data.list[0].dt_txt);
+
+                    console.log(data.list[8].dt_txt);
+
+                    console.log(data.list[16].dt_txt);
+
+                    console.log(data.list[24].dt_txt);
+
+                    console.log(data.list[32].dt_txt);
+
+                    console.log(data.list[39].dt_txt);
+
+
+
+                    //都市ID
+                    console.log(data.city.id);
+
+                    //都市名
+                    console.log(data.city.name);
+
+                    //都市の緯度
+                    console.log(data.city.coord.lat);
+
+                    //都市の経度
+                    console.log(data.city.coord.lon);
+
+                    //都市の属する国(2文字)
+                    console.log(data.city.country);
+
+                    //都市の人口
+                    console.log(data.city.population);
+
+                    //都市のタイムゾーン　unixtime　時差(秒)32400
+                    console.log(data.city.timezone);
+
+                    //都市の日の出時刻　unixtime
+                    console.log(data.city.sunrise);
+                    console.log(data.city.sunrise + data.city.timezone);
+
+                    //都市の日の入り時刻　unixtime
+                    console.log(data.city.sunset);
+                    console.log(data.city.sunset + data.city.timezone);
+
+
+
+
+
 
 
 
@@ -128,35 +172,36 @@ const getPosition = () => {
 
                     //気温
                     const tempAverage = document.querySelector("#js-tempAverage");
-                    tempAverage.innerText = `気温：${JSON.stringify(data.main.temp)}℃`;
+                    tempAverage.innerText = `気温：${JSON.stringify(data.list[0].main.temp)}℃`;
 
                     //最高気温
                     const tempMax = document.querySelector("#js-tempMax");
-                    tempMax.innerText = `最高気温：${JSON.stringify(data.main.temp_max)}℃`;
+                    tempMax.innerText = `最高気温：${JSON.stringify(data.list[0].main.temp_max)}℃`;
 
                     //最低気温
                     const tempMin = document.querySelector("#js-tempMin");
-                    tempMin.innerText = `最低気温：${JSON.stringify(data.main.temp_min)}℃`;
+                    tempMin.innerText = `最低気温：${JSON.stringify(data.list[0].main.temp_min)}℃`;
 
                     //アイコン
                     const weatherIcon = document.querySelector("#js-icon");
                     console.log(weatherIcon);
                     const weatherIconPic = document.createElement("img");
-                    weatherIconPic.src = `img/${JSON.stringify(data.weather[0].icon).replace(/\"/g, "")}@2x.png`;
+                    weatherIconPic.src = `img/${JSON.stringify(data.list[0].weather[0].icon).replace(/\"/g, "")}.svg`;
                     console.log(weatherIconPic);
                     weatherIcon.appendChild(weatherIconPic);
 
+
+
                     //説明
                     const description = document.querySelector("#js-description");
-                    description.innerText = JSON.stringify(data.weather[0].description);
+                    description.innerText = JSON.stringify(data.list[0].weather[0].description);
 
-                    //緯度
-                    const positionLat = document.querySelector("#js-positionLat");
-                    positionLat.innerText = "緯度：" + currentLat;
 
-                    //経度
-                    const positionLon = document.querySelector("#js-positionLon");
-                    positionLon.innerText = "経度：" + currentLon;
+                    //現在地
+                    const currentPosition = document.querySelector("#js-currentPosition");
+                    currentPosition.innerText = "現在地：" + data.city.name;
+
+
 
                     //地図
                     const embedMap = document.querySelector("#js-embedMap");
