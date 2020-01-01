6f0acbdcd6c515159d86036845092fdf506954d7
takeshisakuma <saude0518@gmail.com>
2020-01-01 20:31:01 +0900

    design fix

--- main.js
+++ main.js
@@ -40,6 +40,10 @@ const getPosition = () => {
 
       const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${currentLat}&lon=${currentLon}&appid=${myAppID}&units=${myUnits}&lang=${myLang}`;
 
+
+
+
+
       //getWeather作成
       const getWeather = async () => {
 
@@ -53,148 +57,26 @@ const getPosition = () => {
       getWeather()
         .then(data => {
 
-
-
-
-
-
-
-
-          //ステータス 200以外はエラー？
-          console.log(data.cod);
-
-          //メッセージ
-          console.log(data.message);
-
-          //取得する数
-          console.log(data.cnt);
-
-          //データ受信時刻
-          console.log(data.list[0].dt);
-
           //気温
-          console.log(data.list[0].main.temp);
-
-          //体感気温
-          console.log(data.list[0].main.feels_like);
-
-          //最低気温
-          console.log(data.list[0].main.temp_min);
-
-          //最高気温
-          console.log(data.list[0].main.temp_max);
-
-          //気圧(hPa)
-          console.log(data.list[0].main.pressure);
-
-          //海面の気圧(hPa)
-          console.log(data.list[0].main.sea_level);
-
-          //地面の気圧(hPa)
-          console.log(data.list[0].main.grnd_level);
-
-          //湿度(%)
-          console.log(data.list[0].main.humidity);
-          console.log(data.list[0].main.temp_kf);
-
-          //天気id
-          console.log(data.list[0].weather[0].id);
-
-          //天気main
-          console.log(data.list[0].weather[0].main);
-
-          //天気詳細
-          console.log(data.list[0].weather[0].description);
-
-          //天気アイコン
-          console.log(data.list[0].weather[0].icon);
-
-          //雲の割合？（Cloudiness in %）
-          console.log(data.list[0].clouds.all);
-
-          //風速(meter/sec)
-          console.log(data.list[0].wind.speed);
-
-          //風向き
-          console.log(data.list[0].wind.deg);
-
-
-          console.log(data.list[0].sys.pod);
-
-          //日付と時刻
-          console.log(data.list[0].dt_txt);
-
-          console.log(data.list[8].dt_txt);
-
-          console.log(data.list[16].dt_txt);
-
-          console.log(data.list[24].dt_txt);
-
-          console.log(data.list[32].dt_txt);
-
-          console.log(data.list[39].dt_txt);
-
-
-
-          //都市ID
-          console.log(data.city.id);
-
-          //都市名
-          console.log(data.city.name);
-
-          //都市の緯度
-          console.log(data.city.coord.lat);
-
-          //都市の経度
-          console.log(data.city.coord.lon);
-
-          //都市の属する国(2文字)
-          console.log(data.city.country);
-
-          //都市の人口
-          console.log(data.city.population);
-
-          //都市のタイムゾーン　unixtime　時差(秒)32400
-          console.log(data.city.timezone);
-
-          //都市の日の出時刻　unixtime
-          console.log(data.city.sunrise);
-          console.log(data.city.sunrise + data.city.timezone);
-
-          //都市の日の入り時刻　unixtime
-          console.log(data.city.sunset);
-          console.log(data.city.sunset + data.city.timezone);
-
-
-
-
-
-
-
-
-
-
-
-          //気温
-          const tempAverageGroup = document.querySelectorAll(".js-tempAverage");
-          tempAverageGroup.forEach((value, index) => {
-            tempAverageGroup[index].innerText =
-              JSON.stringify(data.list[index].main.temp)
+          const tempGroup = document.querySelectorAll(".js-temp");
+          tempGroup.forEach((value, index) => {
+            tempGroup[index].innerText =
+              JSON.stringify(data.list[8 * index].main.temp)
           });
 
 
-          //最高気温
-          const tempMaxGroup = document.querySelectorAll(".js-tempMax");
-          tempMaxGroup.forEach((value, index) => {
-            tempMaxGroup[index].innerText =
-              JSON.stringify(data.list[index].main.temp_max);
+          //気圧
+          const pressureGroup = document.querySelectorAll(".js-pressure");
+          pressureGroup.forEach((value, index) => {
+            pressureGroup[index].innerText =
+              JSON.stringify(data.list[8 * index].main.pressure);
           });
 
-          //最低気温
-          const tempMinGroup = document.querySelectorAll(".js-tempMin");
-          tempMinGroup.forEach((value, index) => {
-            tempMinGroup[index].innerText =
-              JSON.stringify(data.list[index].main.temp_min)
+          //湿度
+          const humidityGroup = document.querySelectorAll(".js-humidity");
+          humidityGroup.forEach((value, index) => {
+            humidityGroup[index].innerText =
+              JSON.stringify(data.list[8 * index].main.humidity)
           });
 
 
